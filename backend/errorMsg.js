7d56f96d6509a720f9354cef1d8cc8b732b58918
Sawan <sawankushwah457@gmail.com>
2024-10-09 16:06:25 +0530

    added dynamic error msg

---
+++ backend/errorMsg.js
@@ -0,0 +1,9 @@
+const errorMsgs = {
+    requiredFields: "Site, username, and password are required",
+    internalServerError: "Internal Server Error",
+    passwordNotFound: "Password not found",
+    idRequired: "ID is required",
+    importError: "Error importing the passwords",
+    exportError: "Error exporting the passwords",
+    databaseConnectionFailed: "Database connection failed",
+};

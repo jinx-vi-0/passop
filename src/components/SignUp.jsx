f03bb6c634f19bf5222e77328db62914a0830e58
pani2004 <debashispani61@gmail.com>
2024-10-13 22:48:58 +0530

    Add react-icons package and implement password visibility toggle in SignUp and Sign in component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import { Navigate, Link } from 'react-router-dom'
 import { useAuth } from '../contexts/authContext/index'
 import { doCreateUserWithEmailAndPassword } from '../firebase/auth'
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'
 
 const Register = () => {
-
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setconfirmPassword] = useState('')
     const [isRegistering, setIsRegistering] = useState(false)
     const [errorMessage, setErrorMessage] = useState('')
+    const [showPassword, setShowPassword] = useState(false); 
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     const { userLoggedIn } = useAuth()
 
@@ -28,14 +30,12 @@ const Register = () => {
 
     return (
         <>
-
             <main className="w-full h-screen flex self-center place-content-center place-items-center">
                 <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border rounded-xl">
                     <div className="text-center mb-6">
                         <div className="mt-2">
                             <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">Create a New Account</h3>
                         </div>
-
                     </div>
                     <form
                         onSubmit={onSubmit}
@@ -50,36 +50,48 @@ const Register = () => {
                                 autoComplete='email'
                                 required
                                 value={email} onChange={(e) => { setEmail(e.target.value) }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:green-600 shadow-sm rounded-lg transition duration-300"
+                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-green-600 shadow-sm rounded-lg transition duration-300"
                             />
                         </div>
 
-                        <div>
+                        <div className="relative">
                             <label className="text-sm text-gray-600 font-bold">
                                 Password
                             </label>
                             <input
-                                disabled={isRegistering}
-                                type="password"
+                                type={showPassword ? 'text' : 'password'}
                                 autoComplete='new-password'
                                 required
                                 value={password} onChange={(e) => { setPassword(e.target.value) }}
                                 className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-green-600 shadow-sm rounded-lg transition duration-300"
                             />
+                            <button
+                                type="button"
+                                className="absolute inset-y-0 right-2 top-9"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+                            </button>
                         </div>
 
-                        <div>
+                        <div className="relative">
                             <label className="text-sm text-gray-600 font-bold">
                                 Confirm Password
                             </label>
                             <input
-                                disabled={isRegistering}
-                                type="password"
+                                type={showConfirmPassword ? 'text' : 'password'}
                                 autoComplete='off'
                                 required
                                 value={confirmPassword} onChange={(e) => { setconfirmPassword(e.target.value) }}
                                 className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-green-600 shadow-sm rounded-lg transition duration-300"
                             />
+                            <button
+                                type="button"
+                                className="absolute inset-y-0 right-2 top-9"
+                                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                            >
+                                {showConfirmPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+                            </button>
                         </div>
 
                         {errorMessage && (
